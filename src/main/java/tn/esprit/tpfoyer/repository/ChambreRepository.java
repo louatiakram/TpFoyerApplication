b434a506298673032d540c3f5bfb7cc02223ed97
Akram <akram.louati@esprit.tn>
2024-10-26 22:31:01 +0100

    Update ChambreRepository.java

--- src/main/java/tn/esprit/tpfoyer/repository/ChambreRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/ChambreRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.tpfoyer.entity.Chambre;
 
 public interface ChambreRepository extends JpaRepository<Chambre,Long> {
+    
 }
